3a1309edec73721044603e7c198ba7bc4f809cb6
Daniel Cavalcanti <35306889+danielCavalcanti553@users.noreply.github.com>
2018-12-03 21:34:01 -0200

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -1,13 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { FirebaseConfig } from '../config/firebase.config';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { StorageService } from '../services/storage.service';
+...
 import { GoogleMaps } from '@ionic-native/google-maps';
 import { HttpClientModule } from "@angular/common/http";
 import { MapsService } from '../services/maps.service';
@@ -32,7 +23,6 @@ import { MapsService } from '../services/maps.service';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     AngularFireAuth,
-    StorageService,
     GoogleMaps,
     MapsService
   ]
